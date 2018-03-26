0a173dc0f5d7c242cf45876808e6de9c7fca5ada
Varun Sridharan <varunsridharan23@gmail.com>
2018-03-26 08:35:50 +0530

    Update update.php

--- update.php
+++ update.php
@@ -9,8 +9,8 @@ echo shell_exec("cd ../../");
 echo shell_exec("git clone https://github.com/johnbillion/extended-cpts.git");
 echo shell_exec("cp extended-cpts/src/* ".$dir."/src/");
 echo shell_exec("cp extended-cpts/*.php ".$dir."/");
-//echo shell_exec("rm -r extended-cpts/");
-rmdir($dir.'/extended-cpts/');
+echo shell_exec("rm -r extended-cpts/");
+//rmdir($dir.'/extended-cpts/');
 echo shell_exec("cd ".$dir);
 echo shell_exec("ls");
 
